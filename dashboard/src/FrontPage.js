f23b0e894bef65f34e3e4764e250f34e1e532948
gangisettisumanth <112507977+gangisettisumanth@users.noreply.github.com>
2023-10-31 15:43:30 +0530

    Update FrontPage.js

--- dashboard/src/FrontPage.js
+++ dashboard/src/FrontPage.js
@@ -5,25 +5,6 @@ import FrontScreen from './comopnents/FrontScreen'
 const FrontPage = () => {
     return (
     <>
-    {/* <div class="context">
-        <h1>Pure Css Animated Background</h1>
-    </div>
-
-
-    <div class="area" >
-                <ul class="circles">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                </ul>
-        </div > */}
 
         <FrontScreen/>
     </>
